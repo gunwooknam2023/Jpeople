45761f1978c2b6ad7361a737ff93e1c29b66d2a1
gunwook <taeme0912@naver.com>
2023-09-20 02:32:46 +0900

    [FEAT] init

---
+++ src/main/java/com/example/jpeople/JpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpeople/JpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
