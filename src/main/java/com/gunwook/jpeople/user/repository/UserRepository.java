f46924f59ab080c37dfefd749b929a5422c27a09
gunwook <taeme0912@naver.com>
2023-10-10 21:11:13 +0900

    [FEAT] : 소셜로그인 추가(네이버)

--- src/main/java/com/gunwook/jpeople/user/repository/UserRepository.java
+++ src/main/java/com/gunwook/jpeople/user/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     Optional<User> findByKakaoId(String kakaoId);
     Optional<User> findByGoogleId(String googleId);
+    Optional<User> findByNaverId(String naverId);
 }
