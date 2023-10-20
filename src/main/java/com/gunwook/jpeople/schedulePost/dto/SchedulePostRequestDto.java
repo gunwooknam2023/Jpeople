37aad3767435a1866a3d30fed762789bf1b61c78
gunwook <taeme0912@naver.com>
2023-10-20 18:07:19 +0900

    [FEAT] : 프론트엔드 / 일정카드에 수정, 삭제버튼 추가 + 백엔드 연결

--- src/main/java/com/gunwook/jpeople/schedulePost/dto/SchedulePostRequestDto.java
+++ src/main/java/com/gunwook/jpeople/schedulePost/dto/SchedulePostRequestDto.java
@@ -1,5 +1,7 @@
 package com.gunwook.jpeople.schedulePost.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +12,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SchedulePostRequestDto {
 
+    @NotEmpty(message = "제목을 입력하세요.")
     @Size(max = 10, message = "제목은 10자 이하여야 합니다.")
     private String title;
 
+    @NotEmpty(message = "내용을 입력하세요.")
     @Size(max = 10, message = "내용은 10자 이하여야 합니다.")
     private String contents;
 }
