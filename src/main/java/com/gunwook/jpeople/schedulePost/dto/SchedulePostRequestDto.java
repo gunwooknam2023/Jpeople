a9a2b4cdc3aafdd4b12384e2a7b07f718f4cb5ad
gunwook <taeme0912@naver.com>
2023-10-20 17:16:50 +0900

    [FEAT] : 스케쥴 일정 제목, 내용 글자수 제한

--- src/main/java/com/gunwook/jpeople/schedulePost/dto/SchedulePostRequestDto.java
+++ src/main/java/com/gunwook/jpeople/schedulePost/dto/SchedulePostRequestDto.java
@@ -1,5 +1,6 @@
 package com.gunwook.jpeople.schedulePost.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class SchedulePostRequestDto {
+
+    @Size(max = 10, message = "제목은 10자 이하여야 합니다.")
     private String title;
+
+    @Size(max = 10, message = "내용은 10자 이하여야 합니다.")
     private String contents;
 }
