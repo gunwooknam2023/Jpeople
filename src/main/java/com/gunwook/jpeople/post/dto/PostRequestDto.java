88391a381735766b728a40c1d447a7f1af93abfd
gunwook <taeme0912@naver.com>
2023-10-31 03:47:12 +0900

    [FEAT] : 좋아요 조회 관련 코드수정 + 프론트 앤드 / 자유게시판, 공지사항 조회수+좋아요 표시

--- src/main/java/com/gunwook/jpeople/post/dto/PostRequestDto.java
+++ src/main/java/com/gunwook/jpeople/post/dto/PostRequestDto.java
@@ -2,6 +2,7 @@ package com.gunwook.jpeople.post.dto;
 
 import com.gunwook.jpeople.post.entity.Category;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class PostRequestDto {
+    @Size(max = 20, message = "제목은 최대 20자까지 입력 가능합니다.")
     @NotEmpty(message = "제목을 입력하세요.")
     private String title;
 
