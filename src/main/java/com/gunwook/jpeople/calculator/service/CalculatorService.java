eebab9035f2559d3e34425acaa56cb42237e959b
gunwook <taeme0912@naver.com>
2023-11-14 15:59:37 +0900

    [FEAT] : 계산기 로직 변경 + 예외 상황 추가

--- src/main/java/com/gunwook/jpeople/calculator/service/CalculatorService.java
+++ src/main/java/com/gunwook/jpeople/calculator/service/CalculatorService.java
@@ -4,37 +4,67 @@ import com.gunwook.jpeople.calculator.dto.CalculatorRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Stack;
+
 @Service
 @RequiredArgsConstructor
 public class CalculatorService {
+
     public double calculator(CalculatorRequestDto calculatorRequestDto) {
-        String op = calculatorRequestDto.getCalContents();
+        String calContents = calculatorRequestDto.getCalContents();
+        return evaluateExpression(calContents);
+    }
 
-        String[] addSubParts = op.split("[+\\-]");
-        double result = 0.0;
+    private double evaluateExpression(String expression) {
+        Stack<Double> numbers = new Stack<>();
+        Stack<Character> operators = new Stack<>();
 
-        for (String addSubPart : addSubParts) {
-            String[] mulDivParts = addSubPart.split("[*/]");
-            double mulDivResult = Double.parseDouble(mulDivParts[0]);
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
 
-            for (int i = 1; i < mulDivParts.length; i++) {
-                if (addSubPart.contains("*")) {
-                    mulDivResult *= Double.parseDouble(mulDivParts[i]);
-                } else if (addSubPart.contains("/")) {
-                    mulDivResult /= Double.parseDouble(mulDivParts[i]);
+            if (Character.isDigit(ch)) {
+                StringBuilder num = new StringBuilder();
+                while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
+                    num.append(expression.charAt(i));
+                    i++;
                 }
-            }
+                i--;
 
-            if (op.contains("+")) {
-                result += mulDivResult;
-            } else if (op.contains("-")) {
-                result -= mulDivResult;
-            } else {
-                result = mulDivResult;
+                numbers.push(Double.parseDouble(num.toString()));
+            } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                while (!operators.isEmpty() && hasPrecedence(ch, operators.peek())) {
+                    numbers.push(applyOperator(operators.pop(), numbers.pop(), numbers.pop()));
+                }
+                operators.push(ch);
             }
         }
 
-        return result;
+        while (!operators.isEmpty()) {
+            numbers.push(applyOperator(operators.pop(), numbers.pop(), numbers.pop()));
+        }
+
+        return numbers.pop();
+    }
+
+    private boolean hasPrecedence(char op1, char op2) {
+        return (op2 != '+' && op2 != '-') || (op1 != '*' && op1 != '/');
+    }
+
+    private double applyOperator(char operator, double b, double a) {
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0) {
+                    throw new ArithmeticException("0으로는 나눗셈이 불가능합니다.");
+                }
+                return a / b;
+            default:
+                throw new IllegalArgumentException("기호가 잘못되었습니다.");
+        }
     }
 }
-
