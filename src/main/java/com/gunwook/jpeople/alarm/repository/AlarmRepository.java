bade37ce651d1fb3e0586e21f550d79ca917bfa5
gunwook <taeme0912@naver.com>
2023-11-15 15:39:30 +0900

    [FEAT] : 알림 전송 기능 로직 구현

--- src/main/java/com/gunwook/jpeople/alarm/repository/AlarmRepository.java
+++ src/main/java/com/gunwook/jpeople/alarm/repository/AlarmRepository.java
@@ -1,8 +1,12 @@
 package com.gunwook.jpeople.alarm.repository;
 
 import com.gunwook.jpeople.alarm.entity.Alarm;
+import com.gunwook.jpeople.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+import java.util.List;
 
+public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+    List<Alarm> findByUser(User user);
+    List<Alarm> findByUserAndCheckFalse(User user);
 }
